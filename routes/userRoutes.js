1f86ede7d0b40aeec3ee0783271206b9f5d96a07
ajayaj0302 <ajayaj0302@gmail.com>
2022-04-11 22:31:55 +0530

    Reset password route is fixed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const authController = require("./../controller/authController");
 router.post("/signup", authController.signUp);
 router.post("/login", authController.login);
 router.post("/forgotPassword", authController.forgotPassword);
-router.patch("/resetPassword", authController.resetPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 router.get("/", userController.getAllUsers);
 
